dd687dfe3f7abcaca8d1acb6f692fabbf5bf72e4
Samir Kumar <sam2elect@gmail.com>
2022-01-29 05:16:07 +0530

    Add project files.

---
+++ matrix.Designer.cs
@@ -0,0 +1,170 @@
+﻿using System.Threading;
+namespace Matrix_new
+{
+    partial class matrix
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(matrix));
+            this.Start = new System.Windows.Forms.Button();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.gridSizeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem2 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem3 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem4 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem5 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem6 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem7 = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // Start
+            // 
+            this.Start.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.Start.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.Start.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.Start.ForeColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.Start.Image = ((System.Drawing.Image)(resources.GetObject("Start.Image")));
+            this.Start.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.Start.Location = new System.Drawing.Point(12, 12);
+            this.Start.Name = "Start";
+            this.Start.Size = new System.Drawing.Size(102, 58);
+            this.Start.TabIndex = 0;
+            this.Start.Text = "Start";
+            this.Start.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.Start.UseVisualStyleBackColor = false;
+            this.Start.Click += new System.EventHandler(this.Start_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Dock = System.Windows.Forms.DockStyle.None;
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.gridSizeToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(340, 12);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(91, 28);
+            this.menuStrip1.TabIndex = 3;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // gridSizeToolStripMenuItem
+            // 
+            this.gridSizeToolStripMenuItem.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.gridSizeToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItem2,
+            this.toolStripMenuItem3,
+            this.toolStripMenuItem4,
+            this.toolStripMenuItem5,
+            this.toolStripMenuItem6,
+            this.toolStripMenuItem7});
+            this.gridSizeToolStripMenuItem.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.gridSizeToolStripMenuItem.ForeColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.gridSizeToolStripMenuItem.Name = "gridSizeToolStripMenuItem";
+            this.gridSizeToolStripMenuItem.Size = new System.Drawing.Size(83, 24);
+            this.gridSizeToolStripMenuItem.Text = "Grid size";
+            this.gridSizeToolStripMenuItem.Click += new System.EventHandler(this.gridSizeToolStripMenuItem_Click);
+            // 
+            // toolStripMenuItem2
+            // 
+            this.toolStripMenuItem2.Name = "toolStripMenuItem2";
+            this.toolStripMenuItem2.Size = new System.Drawing.Size(117, 26);
+            this.toolStripMenuItem2.Text = "3*3";
+            this.toolStripMenuItem2.Click += new System.EventHandler(this.toolStripMenuItem2_Click);
+            // 
+            // toolStripMenuItem3
+            // 
+            this.toolStripMenuItem3.Name = "toolStripMenuItem3";
+            this.toolStripMenuItem3.Size = new System.Drawing.Size(117, 26);
+            this.toolStripMenuItem3.Text = "4*4";
+            this.toolStripMenuItem3.Click += new System.EventHandler(this.toolStripMenuItem3_Click);
+            // 
+            // toolStripMenuItem4
+            // 
+            this.toolStripMenuItem4.Name = "toolStripMenuItem4";
+            this.toolStripMenuItem4.Size = new System.Drawing.Size(117, 26);
+            this.toolStripMenuItem4.Text = "5*5";
+            this.toolStripMenuItem4.Click += new System.EventHandler(this.toolStripMenuItem4_Click);
+            // 
+            // toolStripMenuItem5
+            // 
+            this.toolStripMenuItem5.Name = "toolStripMenuItem5";
+            this.toolStripMenuItem5.Size = new System.Drawing.Size(117, 26);
+            this.toolStripMenuItem5.Text = "6*6";
+            this.toolStripMenuItem5.Click += new System.EventHandler(this.toolStripMenuItem5_Click);
+            // 
+            // toolStripMenuItem6
+            // 
+            this.toolStripMenuItem6.Name = "toolStripMenuItem6";
+            this.toolStripMenuItem6.Size = new System.Drawing.Size(117, 26);
+            this.toolStripMenuItem6.Text = "7*7";
+            this.toolStripMenuItem6.Click += new System.EventHandler(this.toolStripMenuItem6_Click);
+            // 
+            // toolStripMenuItem7
+            // 
+            this.toolStripMenuItem7.Name = "toolStripMenuItem7";
+            this.toolStripMenuItem7.Size = new System.Drawing.Size(117, 26);
+            this.toolStripMenuItem7.Text = "8*8";
+            this.toolStripMenuItem7.Click += new System.EventHandler(this.toolStripMenuItem7_Click);
+            // 
+            // matrix
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Info;
+            this.ClientSize = new System.Drawing.Size(1056, 656);
+            this.Controls.Add(this.Start);
+            this.Controls.Add(this.menuStrip1);
+            this.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.Name = "matrix";
+            this.Text = "Matrix";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.OnLoad);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Start;
+        private System.Windows.Forms.Button Pause;
+        private System.Windows.Forms.Button Stop;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem gridSizeToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem2;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem3;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem4;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem5;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem6;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem7;
+        private bool bThreadStatus;
+        public Thread Matrix_new;
+
+    }
+}
+
---
+++ matrix.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace Matrix_new
+{
+    public partial class matrix : Form
+    {
+        public int m_width;
+        public int m_height;
+        public int m_rows;
+        public int m_columns;
+        public int m_Xoffset;
+        public int m_Yoffset;
+        public int m_counter = 2;
+        public int m_size = 8;
+
+        public const int DEFAULT_X_OFFSET = 60;
+        public const int DEFAULT_Y_OFFSET = 100;
+        public const int DEFAULT_NO_ROWS = 2;
+        public const int DEFAULT_NO_COLUMNS = 2;
+        public const int DEFAULT__WIDTH = 40;
+        public const int DEFAULT_HEIGHT = 40;
+        public matrix()
+        {
+            InitializeComponent();
+            Intialize();
+            InitializeComponent();
+            bThreadStatus = false;
+        }
+
+        private void Start_Click(object sender, EventArgs e)
+        {
+            Matrix_new = new Thread(new ThreadStart(ThreadCounter));
+            Matrix_new.Start();
+            bThreadStatus = true;
+        }
+
+        private void Pause_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Stop_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void gridSizeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void toolStripMenuItem2_Click(object sender, EventArgs e)
+        {
+            m_size = 3;
+            this.Refresh();
+        }
+
+        private void toolStripMenuItem3_Click(object sender, EventArgs e)
+        {
+            m_size = 4;
+            this.Refresh();
+        }
+
+        private void toolStripMenuItem4_Click(object sender, EventArgs e)
+        {
+            m_size = 5;
+            this.Refresh();
+        }
+
+        private void toolStripMenuItem5_Click(object sender, EventArgs e)
+        {
+            m_size = 6;
+            this.Refresh();
+        }
+
+        private void toolStripMenuItem6_Click(object sender, EventArgs e)
+        {
+            m_size = 7;
+            this.Refresh();
+        }
+
+        private void toolStripMenuItem7_Click(object sender, EventArgs e)
+        {
+            m_size = 8;
+            this.Refresh();
+        }
+
+        private void OnLoad(object sender, EventArgs e)
+        {
+            DrawGrid();
+        }
+        public void Intialize()
+        {
+            m_rows = DEFAULT_NO_ROWS;
+            m_columns = DEFAULT_NO_COLUMNS;
+            m_width = DEFAULT__WIDTH;
+            m_height = DEFAULT_HEIGHT;
+            m_Xoffset = DEFAULT_X_OFFSET;
+            m_Yoffset = DEFAULT_Y_OFFSET;
+        }
+        private void DrawGrid()
+        {
+            Graphics boardLayout = this.CreateGraphics();
+            Pen layoutPen = new Pen(Color.Red);
+            layoutPen.Width = 3;
+
+            // boardLayout.DrawLine(layoutPen, 0, 0, 100, 0);
+            int X = DEFAULT_X_OFFSET;
+            int Y = DEFAULT_Y_OFFSET;
+
+            for (int i = 0; i <= m_counter; i++)
+            {
+                boardLayout.DrawLine(layoutPen, X, Y, X + this.m_width * this.m_counter, Y);
+                Y = Y + this.m_height;
+            }
+            X = DEFAULT_X_OFFSET;
+            Y = DEFAULT_Y_OFFSET;
+
+            for (int j = 0; j <= m_counter; j++)
+            {
+                boardLayout.DrawLine(layoutPen, X, Y, X, Y + this.m_height * this.m_counter);
+                X = X + this.m_width;
+            }
+        }
+        public void ThreadCounter()
+        {
+            try
+            {
+                while (true)
+                {
+                    m_counter++;
+
+                    if (m_counter > m_size)
+                    {
+                        m_counter = 2;
+                    }
+
+                    Invalidate();
+                    Thread.Sleep(500);
+                }
+            }
+            catch (Exception ex)
+            { }
+        }
+    }
+}
